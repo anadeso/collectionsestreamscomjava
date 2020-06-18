29fd3d389c3cad3f96a59048397cde3e58c731f1
Ana Sousa <anadeso90@gmail.com>
2020-06-18 18:50:08 -0300

    Implementando TreeSet

---
+++ src/set/ExemploTreeSet.java
@@ -0,0 +1,65 @@
+package set;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class ExemploTreeSet {
+    public static void main(String[] args) {
+        TreeSet<String> treeCapitais = new TreeSet<>();
+
+        // Monta a arvore de capitais
+        treeCapitais.add("Porto Alegre");
+        treeCapitais.add("Florianopolis");
+        treeCapitais.add("Curitiba");
+        treeCapitais.add("Sao Paulo");
+        treeCapitais.add("Rio de Janeiro");
+        treeCapitais.add("Belo Horizonte");
+
+        System.out.println(treeCapitais);
+
+        // Retorna a primeira capital no topo da arvore
+        System.out.println(treeCapitais.first());
+
+        // Retorna a ultima capital no final da arvore
+        System.out.println(treeCapitais.last());
+
+        // Retorna a primeira capital abaixo na arvore da capital parametrizada
+        System.out.println(treeCapitais.lower("Florianopolis"));
+
+        // Retorna a primeira capital acima na arvore da capital parametrizada
+        System.out.println(treeCapitais.higher("Florianopolis"));
+
+        // Exibe todas as capitais no console
+        System.out.println(treeCapitais);
+
+        // Retorna a primeira capital no topo da arvore, removendo do set
+        System.out.println(treeCapitais.pollFirst());
+
+        // Retorna a primeira capital no final da arvore, removendo do set
+        System.out.println(treeCapitais.pollLast());
+
+        // Exibe todas as capitais no console
+        System.out.println(treeCapitais);
+
+        // Navega em todos os itens do iterator
+        Iterator<String> iterator = treeCapitais.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        for (String tree : treeCapitais){
+            System.out.println(tree);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
